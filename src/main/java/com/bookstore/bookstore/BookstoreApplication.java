7cd88d35937ff559f4bbe0b449180812dafb7820
Abhigitat <vkchandekar12@gmail.com>
2024-04-24 10:22:00 +0530

    Making Some Changes

--- src/main/java/com/bookstore/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/bookstore/BookstoreApplication.java
@@ -3,7 +3,6 @@ package com.bookstore.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 
